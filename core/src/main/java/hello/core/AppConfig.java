6ce46781218ada878c562fa5ede57c3e9d1ef8a8
MyoungHyeYeon <hyeyeon2964@hanmail.net>
2022-01-10 16:21:37 +0900

    appconfig 수정: 역할 마다 메소드로 나누고 구현체 클래스 생성

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -9,11 +11,18 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
 
-    public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+    public MemberService memberService(){ //멤버서비스 역할
+        return new MemberServiceImpl(memberRepository()); //memberServiceImpl 사용
     }
 
-    public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    private MemberRepository memberRepository() { //멤버레파지토리 역할
+        return new MemoryMemberRepository(); // 메모리로 사용
+    }
+
+    public OrderService orderService(){ //오더서비스 역할
+        return new OrderServiceImpl(memberRepository(), new FixDiscountPolicy());
+    }
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy(); //현재 할인 정책은 정액 할인정책
     }
 }
