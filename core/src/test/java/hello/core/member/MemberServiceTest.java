cc1656afd352a72aa1cd51b2be22c2fccd94755f
MyoungHyeYeon <hyeyeon2964@hanmail.net>
2022-01-10 15:13:26 +0900

    AppConfig에 memberService, orderService 의존성 주입

--- core/src/test/java/hello/core/member/MemberServiceTest.java
+++ core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,14 +1,20 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
+    MemberService memberService;
+    @BeforeEach //테스트를 실행하기 전 무조건 실행 됨
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
     @Test
     void join() {
-
-        MemberService memberService = new MemberServiceImpl();
         //given
         Member member = new Member(1L, "memberA", Grade.VIP);
 
