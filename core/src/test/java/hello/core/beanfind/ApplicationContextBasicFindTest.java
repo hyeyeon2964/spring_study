3880b8d08fa5ecc8e41f2c89a7b3bea213126e67
hyeyeon2964 <hyeyeon2964@hanmail.net>
2022-01-24 16:14:23 +0900

    스프링 빈 조회 기본 - findBeanByName생성

---
+++ core/src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,20 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름 조회")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        System.out.println("memberService = "+ memberService.getClass());
+        System.out.println("memberService.getClass()" + memberService.getClass());
+    }
+}
