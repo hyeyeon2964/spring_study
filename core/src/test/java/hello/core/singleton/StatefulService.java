eda949a7a7ad8590f2a378605ef9eb9181512c56
MyoungHyeYeon <hyeyeon2964@hanmail.net>
2022-02-18 19:09:30 +0900

    싱글톤 객체 무상태성으로 변경

--- core/src/test/java/hello/core/singleton/StatefulService.java
+++ core/src/test/java/hello/core/singleton/StatefulService.java
@@ -3,9 +3,11 @@ package hello.core.singleton;
 public class StatefulService {
     private int price; //상태를 유지하는 필드
     
-    public void order(String name, int price) {
+    public int order(String name, int price) {
         System.out.println("name = " + name + " price" + price);
         this.price = price; //여기가 문제!
+
+        return price;
 
     }
 
--- core/src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ core/src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -15,14 +15,14 @@ class StatefulServiceTest {
         StatefulService statefulService2 = ac.getBean(StatefulService.class);
 
         //ThreadA : A사용자 만원 주문
-        statefulService1.order("userA", 10000);
+        int userAPrice = statefulService1.order("userA", 10000);
         //ThreadB : B사용자 이만원 주문
-        statefulService2.order("userB", 20000);
+        int userBPrice=statefulService2.order("userB", 20000);
 
 
         //ThreadA : 사용자A 주문 금액 조회
         int price = statefulService1.getPrice();
-        System.out.println("price = " + price);
+        System.out.println("price = " + userAPrice);
 
         Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
     }
